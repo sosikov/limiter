0edb9efa2cedf81afde363d2077a78c9d8b3a0ff
sosikov <n.sosikov@gmail.com>
2019-09-17 22:33:17 +0300

    rename variables and methods

--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 const pLimit = require('p-limit');
 const urls = require('./urls');
 
-const randomizer  = () => Math.random() * 5 + 1;
-const ajax = url => new Promise(resolve => setTimeout(() => resolve(url), 3000)); /* randomizer() * 1000 */
-const getDomain = url => url.split('.')[0];
+const ajax = url => new Promise(resolve => setTimeout(() => resolve(url), 1500));
 
 class Limiter {
-  constructor(urls, limitRequests, limitPerDomain) {
-    this.urls = [ ...urls ];
-    this.commonLimiter = pLimit(limitRequests);
-    this.limitPerDomain = limitPerDomain;
+  constructor(urls, commonLimiter, domainLimiter) {
+    this.urls = [...urls];
+    this.commonLimiter = pLimit(commonLimiter);
+    this.domainLimiter = domainLimiter;
     this.domainsLimiters = {};
   }
 
-  request(url, finishResolve) {
-    this.commonLimiter( () => ajax(url).then(res => {
-      console.log('res', res);
-      if (this.commonLimiter.activeCount === 1) {
-        finishResolve();
-      }
-    }) );
-  }
-
-  sendRequests() {
-    return new Promise(finishResolve => {
+  load() {
+    return new Promise(resolve => {
       this.urls.forEach(url => {
-        const domain = getDomain(url);
-  
+        const domain = url.split('.')[0];
+
         if(!this.domainsLimiters[domain]) {
-          this.domainsLimiters[domain] = pLimit(this.limitPerDomain);
+          this.domainsLimiters[domain] = pLimit(this.domainLimiter);
         }
-        this.domainsLimiters[domain]( () => this.request(url, finishResolve) );
+        this.domainsLimiters[domain](() => this.request(url, resolve));
       });
     });
   }
+
+  request(url, resolve) {
+    this.commonLimiter(() => 
+      ajax(url).then(res => {
+        console.log('res >', res);
+        if (this.commonLimiter.activeCount === 1) {
+          resolve();
+        }
+      })
+    );
+  }
 }
 
-const limiter = new Limiter(urls, 10, 3);
-const done = limiter.sendRequests();
-
-done.then(() => console.log('finish!'));
+const loading = new Limiter(urls, 10, 3)
+  .load()
+  .then(() => console.log('loading is complete'));
